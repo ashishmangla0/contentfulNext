9bb71884e1d9cfe1cf5ba5e7df54b9559064045f
ashish <ashishmangla1991@gmail.com>
2021-08-20 14:48:27 +0530

    config for pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
   reactStrictMode: true,
-}
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    scope: '/',
+    sw: 'service-worker.js'
+  }
+})
