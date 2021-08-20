be17c2d5ef3e73b5d55256ffc7e2287bd885cabe
ashish <ashishmangla1991@gmail.com>
2021-08-20 15:04:19 +0530

    add pwa

--- components/jobcard/JobCard.jsx
+++ components/jobcard/JobCard.jsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import style from './jobcard.module.scss';
 const JobCard = ({ job }) => {
   const { jobTitle, excerpt, slug } = job.fields;
-  console.log(job);
   return (
     <div  itemScope itemType="https://schema.org/JobPosting" className={style.jobcard}>
       <h2 itemProp="title">{jobTitle}</h2>
