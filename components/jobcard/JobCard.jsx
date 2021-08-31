c7f9f43b6d0ea1cb672018ab8d44735cbd08a556
ashish <ashishmangla1991@gmail.com>
2021-08-31 12:09:06 +0530

    add more fields

--- components/jobcard/JobCard.jsx
+++ components/jobcard/JobCard.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import style from "./jobcard.module.scss";
 const JobCard = ({ job }) => {
   console.log(job.fields);
-  const { jobTitle, excerpt, slug,location,Dated} = job.fields;
+  const { jobTitle, excerpt, slug,location,Dated,hiringFor} = job.fields;
   return (
     <div
       itemScope
@@ -11,12 +11,24 @@ const JobCard = ({ job }) => {
     >
       <h2 itemProp="title">{jobTitle}</h2>
       <p itemProp="description">{excerpt}</p>
-      <div itemProp="jobLocation" itemScope itemType="https://schema.org/Place">{location}</div>
+      
       <div itemProp="datePosted">{Dated}</div>
 
-      <Link href={`/jobs/${slug}`}>
+      <Link href={`/${slug}`}>
         <a>View Detail</a>
       </Link>
+
+      {hiringFor}
+    <p>
+      <strong>Location:</strong> 
+      <span itemProp="jobLocation" itemScope itemType="https://schema.org/Place">
+        <span itemProp="address" itemScope itemType="https://schema.org/PostalAddress">
+        {location}
+         
+          
+        </span>
+      </span>
+    </p>
     </div>
   );
 };
