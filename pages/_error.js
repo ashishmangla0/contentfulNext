fc5a890a97d0a4d144bc1439ba72a9658e173447
ashish <ashishmangla1991@gmail.com>
2021-09-02 16:41:16 +0530

    add  error pages

--- pages/_error.js
+++ pages/_error.js
@@ -1,7 +1,21 @@
 
+
+import Error404 from '../components/errors/Error404';
+const Error500 = () =>{
+  return<>500</>
+}
+
 const  Error = ({ statusCode })=> {
+  const logStatusCode = {
+    404: <Error404/>,
+    500: <Error500/>
+  };
     return (
       <p>
+       aaaaa {logStatusCode[statusCode]}  <br/>
+        status code {statusCode}
+        
+
         {statusCode
           ? `An error ${statusCode} occurred on server`
           : 'An error occurred on client'}
@@ -13,5 +27,5 @@ const  Error = ({ statusCode })=> {
     const statusCode = res ? res.statusCode : err ? err.statusCode : 404
     return { statusCode }
   }
-  Error.layout = "EL";
+  Error.layout = "ErrorLayout";
   export default Error
