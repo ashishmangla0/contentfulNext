50bdf598261b1c8437ac35204a536128e34cc1bd
ashish <ashishmangla1991@gmail.com>
2021-09-03 15:07:12 +0530

    add detail page

--- pages/jobs/[slug].jsx
+++ pages/jobs/[slug].jsx
@@ -6,26 +6,22 @@ const client = createClient({
 })
 
 
+// export const getStaticPaths = async () => {
+//   const res = await client.getEntries({ content_type: "jobTitle" });
+//   //return { props: { jobs: res.items } };
+//   const paths = res.items.map((item) => {
+//     return {
+//       params: { slug: item.fields.slug },
+//     };
+//   });
 
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
 
-
-
-export const getStaticPaths = async () => {
-  const res = await client.getEntries({ content_type: "jobTitle" });
-  //return { props: { jobs: res.items } };
-  const paths = res.items.map((item) => {
-    return {
-      params: { slug: item.fields.slug },
-    };
-  });
-
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
-export async function getStaticProps({params}) {
+export  const getServerSideProps = async({params}) => {
  
   const {items} = await client.getEntries(
     { content_type: "jobTitle",
